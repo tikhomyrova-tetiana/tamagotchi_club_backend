5bf433579f6782dd8d0b613535f21a2cbecab28b
naomi-eliasar <nreliasar@gmail.com>
2022-06-13 21:32:39 +0200

    /feat creat get routes for club, tamagotchi and users

--- routers/club.js
+++ routers/club.js
@@ -8,9 +8,36 @@ const Tamagotchi = require("../models/").tamagotchi;
 const UserClub = require("../models/").userClub;
 const router = new Router();
 
-//GET all clubs including user model
+//GET all clubs including user and tamagotchi model
+router.get("/", async (req, res, next) => {
+  try {
+    res.send(
+      await Club.findAll({
+        include: [{ model: User }],
+      })
+    );
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
 //GET one specific club including user model
+router.get("/:id", async (req, res, next) => {
+  try {
+    const specificClub = await Club.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+    if (!specificClub) {
+      res.status(404).send(`Club with id ${req.params.id} not found`);
+    } else {
+      res.send(specificClub);
+    }
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
 //PATCH edit club info
 
--- routers/tamagotchi.js
+++ routers/tamagotchi.js
@@ -9,8 +9,35 @@ const UserClub = require("../models/").userClub;
 const router = new Router();
 
 //GET all tamagotchis including user model
+router.get("/", async (req, res, next) => {
+  try {
+    res.send(
+      await Tamagotchi.findAll({
+        include: [{ model: User }],
+      })
+    );
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
 //GET specific tamagotchi including user model
+router.get("/:id", async (req, res, next) => {
+  try {
+    const specificTamagotchi = await Tamagotchi.findByPk(req.params.id, {
+      include: [{ model: User }],
+    });
+    if (!specificTamagotchi) {
+      res.status(404).send(`Tamagotchi with id ${req.params.id} not found`);
+    } else {
+      res.send(specificTamagotchi);
+    }
+  } catch (e) {
+    console.log(e);
+    next(e);
+  }
+});
 
 //POST new tamagotchi
 

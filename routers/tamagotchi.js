c9bd5d1f1dd95f2e1b84e5b1740a9d21e0feda01
rayssalves <rayssa.a.vieira@outlook.com>
2022-06-14 11:34:39 +0200

    patch and post request

--- routers/tamagotchi.js
+++ routers/tamagotchi.js
@@ -13,8 +13,46 @@ const router = new Router();
 //GET specific tamagotchi including user model
 
 //POST new tamagotchi
+router.post("/submitTamaForm", auth, async (request, response, next) => {
+  try {
+    console.log(request.body);
+    const { name, age, deaths, version, generation, evolutionId } =
+      request.body;
 
+    const newForm = await Tamagotchi.create({
+      name: name,
+      age: age,
+      deaths: deaths,
+      version: version,
+      generation: generation,
+      imageUrl: imageUrl,
+      evolutionId: evolutionId,
+    });
+    response.status(201).send(newForm);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 //PATCH tamagotchi info
+router.patch("/edit", auth, async (req, res) => {
+  const { name, age, deaths, version, generation, evolutionId } = req.body;
+  try {
+    const updatedTama = await Tamagotchi.update({
+      name,
+      age: parseInt(age),
+      deaths,
+      version,
+      generation,
+      imageUrl,
+      evolutionId: parseInt(evolutionId),
+    });
+    res.status(200).send({ message: "Profile updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).send("invalid ID");
+  }
+});
 
 //DELETE tamagotchi
 
